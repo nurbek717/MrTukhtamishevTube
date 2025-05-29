baa1332fc1aae1304fb47f6ecfb9edfdfdc1a32d
NurbekTukhtamishev <79134880+nurbek717@users.noreply.github.com>
2025-05-29 16:24:26 +0500

    logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,13 @@
 import { Stack,Box } from '@mui/material'
 import { Link } from 'react-router-dom'
-import { logo } from '../../constants'
+// import { logo } from '../../constants'
 import {SearchBar} from '../'
 import { colors } from '../../constants/colors'
 function Navbar() {
   return (
     <Stack direction={'row'} alignItems={"center"} justifyContent={"space-between"} p={2} sx={{ position: 'sticky', top:0, zIndex:999 , background: colors.primary}}>
       <Link  to={'/'}>
-        <img src={logo} alt="logo"  height={45} width={150} />
+        <img src="./images/MrTukhtamishev-5.svg" alt="logo"  height={45} width={150} />
       </Link>
       <SearchBar/>
     <Box/>
